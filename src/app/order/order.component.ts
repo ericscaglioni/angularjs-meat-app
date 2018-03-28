ab2e612a92032b485d7567a2d8725c0c4d84092f
ericscaglioini <ericsf34@gmail.com>
2018-03-27 21:17:52 -0300

    Finalizando componente de compra

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from '../radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mt-order',
@@ -17,7 +19,8 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value: 'REF'}
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -40,6 +43,17 @@ export class OrderComponent implements OnInit {
 
   itemsValue(): number {
     return  this.orderService.itemsValue();
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems()
+      .map((item:CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+
+    this.orderService.checkOrder(order)
+      .subscribe((orderId: string) => {
+        this.router.navigate(['/order-summary'])
+        this.orderService.clear()
+      })
   }
 
 }
