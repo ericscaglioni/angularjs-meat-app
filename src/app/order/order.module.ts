0b7787249193f6df57bd93743a0f895360a0d6a2
ericscaglioini <ericsf34@gmail.com>
2018-04-02 22:16:54 -0300

    Criação de módulos para separar melhor a aplicação juntamente com o conceito de Lazy Loading

---
+++ src/app/order/order.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { OrderComponent } from './order.component';
+import { OrderItemsComponent } from './order-items/order-items.component';
+import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
+import { SharedModule } from '../shared/shared.module';
+import { Routes, RouterModule } from '@angular/router';
+
+const ROUTES: Routes = [
+  { path: '', component: OrderComponent }
+]
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild(ROUTES)
+  ],
+  declarations: [
+    OrderComponent,
+    OrderItemsComponent,
+    DeliveryCostsComponent
+  ]
+})
+export class OrderModule { }
